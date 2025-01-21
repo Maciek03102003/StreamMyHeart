31fa6e83d5ea748584cd5058ab78038b494b1db8
LorinaPan <arriky_pan@outlook.com>
2025-01-21 22:18:07 +0000

    fix: attempt to setup package parameter correctly

--- .github/scripts/Package-Windows.ps1
+++ .github/scripts/Package-Windows.ps1
@@ -82,7 +82,6 @@ function Package {
     Log-Group
 
     Log-Information "Debug: Packaging ${ProductName}..."
-    $BuildInstaller = $true
     Log-Information "Debug: the build installer flag is set to ${BuildInstaller}"
     if ( ( $BuildInstaller ) ) {
         Log-Information "Debug: Packaging ${ProductName} with installer..."
