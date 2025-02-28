67d562b9587cbe67e27a083ea6209d7c91283dde
Maciej Rytlewski <maciekrytlewski@gmail.com>
2025-02-28 11:28:39 +0000

    [Feature] Add checks for pre-filtering, PPG algorithms, post-filtering, and smoothing in rank_csv_files.py, to allow us to compare the effect of each of them

--- eval/rank_csv_files.py
+++ eval/rank_csv_files.py
@@ -12,16 +12,106 @@ def calculate_mean_rmse(file_path):
     return np.mean(rmses)
 
 def rank_csv_files(directory):
-    rmse_means = {}
+    rmseMeans = {}
+    haarCascadeRmse = []
+    dlibRmse = []
+    preFilteringNoneRmse = []
+    preFilteringBandpassRmse = []
+    preFilteringDetrendRmse = []
+    preFilteringZeroMeanRmse = []
+    greenRmse = []
+    pcaRmse = []
+    chromRmse = []
+    postFilteringNoneRMSE = []
+    postFilteringBandpassRMSE = []
+    smoothingOnRmse = []
+    smoothingOffRmse = []
     for filename in os.listdir(directory):
         if filename.endswith('.csv'):
-            file_path = os.path.join(directory, filename)
-            mean_rmse = calculate_mean_rmse(file_path)
-            rmse_means[filename] = mean_rmse
+            filePath = os.path.join(directory, filename)
+            parts = filename.split('_')
+            meanRmse = calculate_mean_rmse(filePath)
+            rmseMeans[filename] = meanRmse
+            
+            if "HAAR_CASCADE" in filename:
+                haarCascadeRmse.append(meanRmse)
+            elif "DLIB" in filename:
+                dlibRmse.append(meanRmse)
+                
+            preFiltering = 0
+            if "HAAR_CASCADE" in filename:
+                preFiltering = parts[2]
+            else:
+                preFiltering = parts[1]
 
-    ranked_files = sorted(rmse_means.items(), key=lambda x: x[1])
-    for rank, (filename, mean_rmse) in enumerate(ranked_files, start=1):
-        print(f"{rank}. {filename}: {mean_rmse:.4f}")
+            print(preFiltering)
+                
+            if preFiltering == "NONE":
+                preFilteringNoneRmse.append(meanRmse)
+            elif preFiltering == "BUTTERWORTH":
+                preFilteringBandpassRmse.append(meanRmse)
+            elif preFiltering == "DETREND":
+                preFilteringDetrendRmse.append(meanRmse)
+            elif preFiltering == "ZERO":
+                preFilteringZeroMeanRmse.append(meanRmse)
+                
+            if "GREEN" in filename:
+                greenRmse.append(meanRmse)
+            elif "PCA" in filename:
+                pcaRmse.append(meanRmse)
+            elif "CHROM" in filename:
+                chromRmse.append(meanRmse)
+                
+            postFiltering = parts[-3]
+            if postFiltering == "BANDPASS":
+                postFilteringBandpassRMSE.append(meanRmse)
+            elif postFiltering == "NONE":
+                postFilteringNoneRMSE.append(meanRmse)
+            
+            if "SMOOTHING_ON" in filename:
+                smoothingOnRmse.append(meanRmse)
+            elif "SMOOTHING_OFF" in filename:
+                smoothingOffRmse.append(meanRmse)
 
+    rankedFiles = sorted(rmseMeans.items(), key=lambda x: x[1])
+    for rank, (filename, meanRmse) in enumerate(rankedFiles, start=1):
+        print(f"{rank}. {filename}: {meanRmse:.4f}")
+    
+    if haarCascadeRmse and dlibRmse:
+        meanRmseHaar = np.mean(haarCascadeRmse)
+        meanRmseDlib = np.mean(dlibRmse)
+        print(f"\nMean RMSE with Haar cascade: {meanRmseHaar:.4f}")
+        print(f"Mean RMSE with DLIB: {meanRmseDlib:.4f}")
+        
+    if preFilteringNoneRmse and preFilteringBandpassRmse and preFilteringDetrendRmse and preFilteringZeroMeanRmse:
+        meanRmsePreFilteringNone = np.mean(preFilteringNoneRmse)
+        meanRmsePreFilteringBandpass = np.mean(preFilteringBandpassRmse)
+        meanRmsePreFilteringDetrend = np.mean(preFilteringDetrendRmse)
+        meanRmsePreFilteringZeroMean = np.mean(preFilteringZeroMeanRmse)
+        print(f"\nMean RMSE with pre-filtering none: {meanRmsePreFilteringNone:.4f}")
+        print(f"Mean RMSE with pre-filtering bandpass: {meanRmsePreFilteringBandpass:.4f}")
+        print(f"Mean RMSE with pre-filtering detrend: {meanRmsePreFilteringDetrend:.4f}")
+        print(f"Mean RMSE with pre-filtering zero mean: {meanRmsePreFilteringZeroMean:.4f}")
+    
+    if greenRmse and pcaRmse:
+        meanRmseGreen = np.mean(greenRmse)
+        meanRmsePca = np.mean(pcaRmse)
+        meanRmseChrom = np.mean(chromRmse)
+        print(f"\nMean RMSE with Green: {meanRmseGreen:.4f}")
+        print(f"Mean RMSE with PCA: {meanRmsePca:.4f}")
+        print(f"Mean RMSE with Chrom: {meanRmseChrom:.4f}")
+        
+    if postFilteringBandpassRMSE and postFilteringNoneRMSE:
+        meanRmsePostFilterNone = np.mean(postFilteringNoneRMSE)
+        meanRmsePostFilterBandpass = np.mean(postFilteringBandpassRMSE)
+        print(f"\nMean RMSE with post-filtering none: {meanRmsePostFilterNone:.4f}")
+        print(f"Mean RMSE with post-filtering bandpass: {meanRmsePostFilterBandpass:.4f}")
+        
+    if smoothingOnRmse and smoothingOffRmse:
+        meanRmseSmoothingOff = np.mean(smoothingOffRmse)
+        meanRmseSmoothingOn = np.mean(smoothingOnRmse)
+        print(f"\nMean RMSE with smoothing off: {meanRmseSmoothingOff:.4f}")
+        print(f"Mean RMSE with smoothing on: {meanRmseSmoothingOn:.4f}")
+        
 if __name__ == "__main__":
     rank_csv_files('new_results')
